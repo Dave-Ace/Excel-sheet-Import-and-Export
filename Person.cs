d041ae6f62017164f3ed82640f4f3cfc65cac7b1
David <davidoyekanmi.t@gmail.com>
2023-04-27 12:08:23 +0100

    Extracting excel data into object

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_sheet_Import_and_Export
+{
+    public class Person
+    {
+        public string FirstName { get; set; }  
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string Mobile { get; set; }
+        public string State { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using OfficeOpenXml;
+using System.Drawing.Text;
+namespace Excel_sheet_Import_and_Export
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\Tenece\Desktop\data.xlsx";
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                var sheet = package.Workbook.Worksheets[0];
+                var persons = new Program().GetList<Person>(sheet);
+                foreach (Person person in persons)
+                {
+                    Console.WriteLine($"{person.FirstName} \t {person.LastName} \t {person.Sex} \t {person.Mobile} \t {person.State}");
+                }
+            }
+        }
+
+        private List<T> GetList<T>(ExcelWorksheet sheet)
+        {
+            List<T> list = new List<T>();
+            var columnInfo = Enumerable.Range(1, sheet.Dimension.Columns).ToList().Select(n =>
+                
+                new { Index = n, ColumnName = sheet.Cells[1, n].Value.ToString()}
+            );
+            for(int row =2; row < sheet.Dimension.Rows; row++)
+            {
+                T obj = (T)Activator.CreateInstance(typeof(T)); //generic object
+                foreach(var prop in typeof(T).GetProperties())
+                {
+                    int col = columnInfo.SingleOrDefault(c => c.ColumnName == prop.Name).Index;
+                    var val = sheet.Cells[row, col].Value;
+                    var propType = prop.PropertyType;
+                    prop.SetValue(obj, Convert.ChangeType(val, propType));
+                }
+                list.Add(obj);
+            }
+            return list;
+        }
+    }
+}
+
+
